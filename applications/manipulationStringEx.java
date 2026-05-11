e6122c854717d8870e500780f64e1b6a97ef3d3a
Sara Salaheddin <152617750+SaraSalaheddinZa@users.noreply.github.com>
2026-05-12 00:00:46 +0300

    Add files via upload

---
+++ applications/manipulationStringEx.java
@@ -0,0 +1,18 @@
+public class manipulationStringEx {
+    public static void main (String[] args){
+        // First Example:
+        String stringToBeDisplayed = "Next year, Please";
+        String howWasLastYear = "He was good in his activity";
+        if (howWasLastYear.contains("good")){
+            stringToBeDisplayed = "You get a gift from santa";
+        }
+        System.out.println("Santa's Result: " + stringToBeDisplayed);
+
+        // Second Example:
+        String name1 = "santa";
+        String name2 = "SANTA";
+        if (name1.equalsIgnoreCase(name2)){
+            System.out.println("The names are the same");
+        }
+    }
+}
