9ac8818f86fc114c5d85c101f7c7e6d4b7386582
Sara Salaheddin <152617750+SaraSalaheddinZa@users.noreply.github.com>
2026-06-09 02:18:48 +0300

    Add files via upload

---
+++ applications/implement-multiple-inheritance/KungFuSkills.java
@@ -0,0 +1,7 @@
+/* get inside the curly braces of the interface  in order
+        to declare a method named "kungFuProtection()".
+        Ensure that the  return type of the method is "void".
+ */
+public interface KungFuSkills {
+    void kungFuProtection();
+}
